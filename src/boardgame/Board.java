57ccb1a84f96a0ab039714d1efe74ae21af47265
JoaoSobrinhoMoD <joaosobrinho9000@gmail.com>
2024-07-03 21:09:19 -0300

    Moving Pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -44,6 +44,19 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece; // atribuindo a peça informada
 		piece.position = position;
 	}
+	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 
 	private boolean positionExists(Integer row, Integer column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
